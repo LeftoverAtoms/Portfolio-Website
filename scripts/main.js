14662bbd4a82f616285a2f64731ad285ececb248
Adam <adamjasoncalvelage@gmail.com>
2023-05-06 23:05:28 -0400

    Major Refactor
    -Website is now centered with a base resolution of ~1280x720.
    -Begun implementing selectable blogs.
    -Added a neat little animation for interactables.
    -Resolved errors (included setting up a favicon.ico)

--- scripts/main.js
+++ scripts/main.js
@@ -1,47 +1,42 @@
-function PopulateBlogs( iBlogs )
+async function LoadProfile()
 {
-    //const layout = [];
-    //layout.push('Website Prototype', './images/thumbnails/socket-thumbnail.png"', 'Today we are writing a website from scratch as a portpolio piece');
+	const element = document.querySelector('.profile');
 
-    const root = document.querySelector('.blog');
-    for (let i = 0; i < iBlogs; i++)
-    {
-        // Create and define a new blog element
-        let blog = document.createElement('div');
-        blog.classList.add('blog-post');
-    }
+	const data = await fetch('./data/profile.json').then( res => res.json());
+
+	return element.innerHTML = `
+		<div class="identity">
+			<img class="portrait" src='${data.portrait}' />
+			<span class="name">${data.name}</span>
+			<div class="social-links">
+				<a href="${data.links.github.link}"><img src="${data.links.github.icon}" alt="Github logo"></a>
+				<a href="${data.links.twitter.link}"><img src="${data.links.twitter.icon}" alt="Twitter Logo"></a>
+			</div>
+		</div>
+		<span class="bio">${data.bio}</span>
+		<div class="skills">
+			<span class="skills-title">Skills</span>
+			<div class="skills-items">
+				${data.skills.map(skill => `<span class="skill-item">${skill}</span>`).join('\n')}
+			</div>
+		</div>
+	`;
 }
 
-function FetchForm()
+function RemoveBlogs()
 {
-    const form = document.querySelectorAll('formlmao');
-
-    print(form);
+	const x = document.querySelector('.blogs');
+	while (x.firstChild)
+	{
+		x.removeChild(x.lastChild);
+	}
 }
 
-const profile = document.querySelector('.profile')
+function LoadBlog(strID) {
 
-async function LoadContent() {
-    const response = await fetch('./data/profile.json');
-    const data = await response.json();
-
-    profile.innerHTML = `
-        <div class="profile-content">
-            <img class="portrait" src='${data.portrait}' />
-            <span class="name">${data.name}</span>
-            <div class="social-links">
-                <a href="${data.links.github.link}"><img src="${data.links.github.icon}" alt="Github logo"></a>
-                <a href="${data.links.twitter.link}"><img src="${data.links.twitter.icon}" alt="Twitter Logo"></a>
-            </div>
-        </div>
-        <span class="bio">${data.bio}</span>
-        <div class="skills">
-            <span class="skills-title">Skills</span>
-            <div class="skills-items">
-                ${data.skills.map(skill => `<span class="skill-item">${skill}</span>`).join('\n')}
-            </div>
-        </div>
-    `
+	const element = document.getElementById(strID);
+	console.log(element);
+	
 }
 
-LoadContent();
+LoadProfile();
