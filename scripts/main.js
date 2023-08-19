6c3b6caefae199e29850b1addb1fddafb4bd99fa
Adam <adamjasoncalvelage@gmail.com>
2023-08-19 01:49:41 -0400

    REWORK
    - Rework everything
    - Insert dev visuals

--- scripts/main.js
+++ scripts/main.js
@@ -1,37 +1,18 @@
-async function LoadProfile()
-{
-	const element = document.querySelector('.profile');
-
-	const data = await fetch('./data/profile.json').then( res => res.json());
-
-	return element.innerHTML = `
-		<div class="identity">
-			<img class="portrait" src='${data.portrait}' />
-			<span class="name">${data.name}</span>
-			<span class="career">${data.career}</span>
-		</div>
-		<div class="skills">
-			<div class="skills-items">
-				${data.skills.map(skill => `<span class="skill-item">${skill}</span>`).join('\n')}
-			</div>
-		</div>
-	`;
-}
-
 function RemoveBlogs()
 {
-	const x = document.querySelector('.blogs');
-	while (x.firstChild)
-	{
-		x.removeChild(x.lastChild);
+	const x = document.querySelector('.blog-content');
+
+	console.log(x.children.length);
+
+	for (let i = 0; i < x.children.length; i++) {
+		if (x.children[i].className == "blog-entry") {
+			x.children[i].style.display = "none";
+		}
 	}
 }
 
-function LoadBlog(strID) {
-
-	const element = document.getElementById(strID);
-	console.log(element);
+function LoadBlog() {
+	document.querySelector(".blog").style.display = "initial";
 	
+	RemoveBlogs();
 }
-
-LoadProfile();
